e073e220e447235050e92e344191e4f5e50f7ca1
pius <piuskariuki19@gmail.com>
2024-07-02 12:50:44 +0300

    (chore) Renamed patient-flags to esm-nav-app

--- packages/esm-nav-app/src/index.ts
+++ packages/esm-nav-app/src/index.ts
@@ -1,10 +1,8 @@
 import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
 import { configSchema } from './config-schema';
-import kenyaEmr2ChartLinkComponent from './kenyaemr-link/kenyaemr-chart-link.component';
-import patientFlagsComponent from './patient-flags/patient-flags.component';
 import NavbarActionButton from './navbar/navbar-action-button.component';
 
-const moduleName = '@kenyaemr/esm-patient-flags-app';
+const moduleName = '@kenyaemr/esm-nav-app';
 
 const options = {
   featureName: 'patient-flags',
@@ -13,8 +11,6 @@ const options = {
 
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 export const navbarButtons = getSyncLifecycle(NavbarActionButton, options);
-export const kenyaemr2ChartLink = getSyncLifecycle(kenyaEmr2ChartLinkComponent, options);
-export const patientFlag = getSyncLifecycle(patientFlagsComponent, options);
 
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
