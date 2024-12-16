3d86d7aec43b5cf96113c40bbb24660a66f2e56d
Munyua123 <earlmunyua96@gmail.com>
2024-12-16 14:47:06 +0300

    SJT-127 Billing module When navigating the side navigation options in the billing module only the first tab,Billing overview remains highlighted.instead of the current active one.

--- packages/esm-billing-app/src/payment-modes/payment-mode-home.component.tsx
+++ packages/esm-billing-app/src/payment-modes/payment-mode-home.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BillingHeader from '../billing-header/billing-header.component';
 import { useTranslation } from 'react-i18next';
 import PaymentModeDashboard from './payment-mode-dashboard.component';
+import LeftPanel from '../left-panel/left-panel.component';
 
 type PaymentModeHomeProps = {};
 
@@ -9,6 +10,7 @@ const PaymentModeHome: React.FC<PaymentModeHomeProps> = () => {
   const { t } = useTranslation();
   return (
     <>
+    <LeftPanel />
       <BillingHeader title={t('paymentModes', 'Payment Modes')} />
       <PaymentModeDashboard />
     </>
