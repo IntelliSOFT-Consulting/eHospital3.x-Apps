66e7bac9d24fa28817882c69bcc091772f73d1b4
Samuel Muli <samuelmuli95@gmail.com>
2025-01-23 12:08:31 +0300

    Create ai model skeleton

---
+++ packages/esm-ai-model-app/src/ai-model/ai-model.workspace.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './ai-model.scss'
+
+const AIModel: React.FC = () => {
+  return (
+		<div className={styles.container}>
+			<h1>AI Model</h1>
+		</div>
+	)
+}
+
+export default AIModel
