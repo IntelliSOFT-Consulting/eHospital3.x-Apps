bda996b13c02aff643fa79541c968559e2f1ed5b
Samuel Muli <samuelmuli95@gmail.com>
2025-03-05 12:09:49 +0300

    Implement llm functionality

--- packages/esm-ai-model-app/src/ai-model/ai-model.workspace.tsx
+++ packages/esm-ai-model-app/src/ai-model/ai-model.workspace.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import styles from './ai-model.scss'
 import { 
-	Button
+	Button,
+	Loading
 } from "@carbon/react";
 import { SendAlt } from "@carbon/react/icons";
 import PrimaryButton from "../components/primary-button.component";
-import { useLaunchWorkspaceRequiringVisit } from "@openmrs/esm-patient-common-lib";
+import { getPatientUuidFromUrl, useLaunchWorkspaceRequiringVisit } from "@openmrs/esm-patient-common-lib";
 import CustomTextArea from "../components/text-area.component";
 import GeneratedResponse from "../components/generated-response.component";
 import Feedback from "../components/feedback.component";
+import { useObservations } from "../hooks/useObservations";
 
 
 const AIModel: React.FC = () => {
 	const [isEditMode, setEditMode] = useState(false);
 	const [isRegenerated, setRegenerated] = useState(false);
 	const launchAiModelGeneratedWorkSpace = useLaunchWorkspaceRequiringVisit('ai-model-generated');
+	const patientUuid = getPatientUuidFromUrl()
+	const {
+		data: observations,
+		llmResponse,
+		isGenerating,
+		generateLlmMessage
+	} = useObservations(patientUuid);
 
 	const toggleEditMode = () => {
 		setEditMode(true);
@@ -28,42 +37,80 @@ const AIModel: React.FC = () => {
 		<div className={styles.container}>
 			<div className={styles.body}>
 				<div className={styles.summary}>
-					<p>Hey there, here is a summary of this encounter</p>
-					<section>
-						<p>This section will contain a summary of biometrics and diagnosis</p>
-					</section>
-					<section>
-						<p>
-							This section will contain a summary of patient's diagnosis according to the last visit.
-							It will also give a summary of how the medical practitioner reached to the conclusion mentioned above.
-						</p>
-					</section>
-					<section>
-						<p>
-							This section will show the common medications and recommendations for each diagnosis given above.
-						</p>
-					</section>
+					{observations && <h1 className={styles.title}>Patient Encounter Summary</h1>}
+
+					{!observations && (
+						<div className={styles.loading}>
+							<Loading 
+								small 
+								withOverlay={false} 
+							/>
+							<p>Loading observations...</p>
+						</div>
+					)}
+					{observations?.map((obs, index) => (
+            <div key={index} className={styles.observation}>
+              <div>
+								<p>Sex: {obs.sex}</p>
+								<p>Age: {obs.age}</p>
+								<p>Weight: {obs.weight}</p>
+								<p>Height: {obs.height}</p>
+								<p>BMI: {obs.bmi}</p>
+								<p>Blood Pressure: {obs.bloodPressure}</p>
+								<p>Heart Rate: {obs.heartRate}</p>
+								<p>Temperature: {obs.temperature}</p>
+								<p>Diagnosis: {obs.diagnosis}</p>
+							</div>
+              {obs.medication && obs.medication.length > 0 && (
+								<>
+									<p>Medications: </p>
+                	<p>{obs.medication.join(", ")}</p>
+								</>
+              )}
+              {obs.tests && obs.tests.length > 0 && (
+                <div>
+                  <p>Tests:</p>
+                  {obs.tests.map((test, testIndex) => (
+                    <div key={testIndex}>
+                      <strong>{test.test_name}</strong>
+                      {test.test_results?.map((result, resultIndex) => (
+                        <div key={resultIndex}>
+                          {result.parameter}: {result.value}
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              )}
+              {obs.condition && obs.condition.length > 0 && (
+                <>
+									<p>Conditions: </p>
+									<p>{obs.condition.join(", ")}</p>
+								</>
+              )}
+            </div>
+          ))}
 				</div>
 
-				<Button
+				{observations && (<Button
 					renderIcon={SendAlt}
 					kind="secondary"
 					size="sm"
 				>
 					Generate Message
-				</Button>
+				</Button>)}
 			</div>
 
 			<div className={styles.chat}>
 
-				{!isEditMode && <GeneratedResponse toggleEditMode={toggleEditMode} />}
+				{!isEditMode && observations && <GeneratedResponse toggleEditMode={toggleEditMode} />}
 
-				{isEditMode && <CustomTextArea />}
+				{isEditMode && observations && <CustomTextArea />}
 
-				<div className={styles.actions}>
+				{observations && (<div className={styles.actions}>
 					<PrimaryButton onClick={launchAiModelGeneratedWorkSpace}>Approve</PrimaryButton>
 					<PrimaryButton onClick={toggleRegenerate}>Regenerate</PrimaryButton>
-				</div>
+				</div>)}
 			</div>
 
 			{isRegenerated && <Feedback title="Reason for Regeneration" />}
