80ae84cdd661ca4ed0a141045ae9ba71eb5324f6
Your Name <michaelndula@gmail.com>
2025-03-10 15:14:58 +0300

    Get Patients Visit observations

--- packages/esm-ai-model-app/src/ai-model/ai-model.workspace.tsx
+++ packages/esm-ai-model-app/src/ai-model/ai-model.workspace.tsx
@@ -6,7 +6,7 @@ import {
 } from "@carbon/react";
 import { SendAlt } from "@carbon/react/icons";
 import PrimaryButton from "../components/primary-button.component";
-import { getPatientUuidFromUrl, useLaunchWorkspaceRequiringVisit } from "@openmrs/esm-patient-common-lib";
+import { getPatientUuidFromStore, useLaunchWorkspaceRequiringVisit } from "@openmrs/esm-patient-common-lib";
 import CustomTextArea from "../components/text-area.component";
 import GeneratedResponse from "../components/generated-response.component";
 import Feedback from "../components/feedback.component";
@@ -17,7 +17,7 @@ const AIModel: React.FC = () => {
 	const [isEditMode, setEditMode] = useState(false);
 	const [isRegenerated, setRegenerated] = useState(false);
 	const launchAiModelGeneratedWorkSpace = useLaunchWorkspaceRequiringVisit('ai-model-generated');
-	const patientUuid = getPatientUuidFromUrl()
+	const patientUuid = getPatientUuidFromStore()
 	const {
 		data: observations,
 		llmResponse,
@@ -36,60 +36,90 @@ const AIModel: React.FC = () => {
   return (
 		<div className={styles.container}>
 			<div className={styles.body}>
-				<div className={styles.summary}>
-					{observations && <h1 className={styles.title}>Patient Encounter Summary</h1>}
+			<div className={styles.summary}>
+      {observations && <h1 className={styles.title}>Patient Encounter Summary</h1>}
 
-					{!observations && (
-						<div className={styles.loading}>
-							<Loading 
-								small 
-								withOverlay={false} 
-							/>
-							<p>Loading observations...</p>
-						</div>
-					)}
-					{observations?.map((obs, index) => (
-            <div key={index} className={styles.observation}>
-              <div>
-								<p>Sex: {obs.sex}</p>
-								<p>Age: {obs.age}</p>
-								<p>Weight: {obs.weight}</p>
-								<p>Height: {obs.height}</p>
-								<p>BMI: {obs.bmi}</p>
-								<p>Blood Pressure: {obs.bloodPressure}</p>
-								<p>Heart Rate: {obs.heartRate}</p>
-								<p>Temperature: {obs.temperature}</p>
-								<p>Diagnosis: {obs.diagnosis}</p>
-							</div>
-              {obs.medication && obs.medication.length > 0 && (
-								<>
-									<p>Medications: </p>
-                	<p>{obs.medication.join(", ")}</p>
-								</>
-              )}
-              {obs.tests && obs.tests.length > 0 && (
-                <div>
-                  <p>Tests:</p>
-                  {obs.tests.map((test, testIndex) => (
-                    <div key={testIndex}>
-                      <strong>{test.test_name}</strong>
-                      {test.test_results?.map((result, resultIndex) => (
-                        <div key={resultIndex}>
-                          {result.parameter}: {result.value}
-                        </div>
-                      ))}
-                    </div>
-                  ))}
-                </div>
-              )}
-              {obs.condition && obs.condition.length > 0 && (
-                <>
-									<p>Conditions: </p>
-									<p>{obs.condition.join(", ")}</p>
-								</>
-              )}
+      {!observations && (
+        <div className={styles.loading}>
+          <Loading small withOverlay={false} />
+          <p>Loading observations...</p>
+        </div>
+      )}
+      
+      {observations?.map((obs, index) => {
+        const hasData =
+          obs.gender ||
+          obs.age ||
+          obs.weight ||
+          obs.height ||
+          obs.bmi ||
+          obs.bloodPressure ||
+          obs.heartRate ||
+          obs.temperature ||
+          obs.diagnosis ||
+          (obs.medication && obs.medication.length > 0) ||
+          (obs.tests && obs.tests.length > 0) ||
+          (obs.condition && obs.condition.length > 0);
+
+        if (!hasData) {
+          return null;
+        }
+
+        return (
+          <div key={index} className={styles.observation}>
+            <div>
+              {obs.gender && <p>Sex: {obs.gender}</p>}
+              {obs.age && <p>Age: {obs.age}</p>}
+              {obs.weight && <p>Weight: {obs.weight}</p>}
+              {obs.height && <p>Height: {obs.height}</p>}
+              {obs.bmi && <p>BMI: {obs.bmi}</p>}
+              {obs.bloodPressure && <p>Blood Pressure: {obs.bloodPressure}</p>}
+              {obs.heartRate && <p>Heart Rate: {obs.heartRate}</p>}
+              {obs.temperature && <p>Temperature: {obs.temperature}</p>}
+              {obs.diagnosis && (
+				<div>
+					<p>Diagnosis:</p>
+					<ul>
+					{obs.diagnosis
+						.replace(/[\[\]]/g, '')
+						.split(',')
+						.map((diag, index) => (
+						<li key={index}>{diag.trim()}</li>
+						))}
+					</ul>
+				</div>
+				)}
             </div>
-          ))}
+            {obs.medication && obs.medication.length > 0 && (
+              <>
+                <p>Medications:</p>
+                <p>{obs.medication.join(", ")}</p>
+              </>
+            )}
+            {obs.tests && obs.tests.length > 0 && (
+			<div>
+				<p>Tests:</p>
+				{obs.tests.map((test, testIndex) => (
+				<div key={testIndex}>
+					<strong>{test.name}</strong>
+					{test.results?.map((result, resultIndex) => (
+					<div key={resultIndex}>
+						{result.parameter}: {result.value}
+					</div>
+					))}
+				</div>
+				))}
+			</div>
+			)}
+            {obs.condition && obs.condition.length > 0 && (
+              <>
+                <p>Conditions:</p>
+                <p>{obs.condition.join(", ")}</p>
+              </>
+            )}
+          </div>
+        );
+      })}
 				</div>
 
 				{observations && (<Button
