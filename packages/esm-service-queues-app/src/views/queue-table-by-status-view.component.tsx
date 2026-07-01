e539e286bf2d5e62855b5420096f0b6ee271b8a0
mutajonathan <mutajonathan4@gmail.com>
2026-07-01 14:04:21 +0300

    refactor: remove unused import of QueueTableByStatus component in queue-table-by-status-view

--- packages/esm-service-queues-app/src/views/queue-table-by-status-view.component.tsx
+++ packages/esm-service-queues-app/src/views/queue-table-by-status-view.component.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { useQueue } from '../hooks/useQueue';
-import QueueTableByStatus from '../queue-table/queue-table-by-status.component';
 import { QueueTableByStatusSkeleton } from '../queue-table/queue-table-by-status-skeleton.component';
 import QueueTablesForAllStatuses from './queue-tables-for-all-statuses.component';
 
